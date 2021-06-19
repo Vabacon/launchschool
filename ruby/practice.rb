eb3f0ddd401211e626910b58698af98710f58307
Vinton Bacon <vintonabacon@gmail.com>
2021-06-19 13:05:15 -0500

    updated .md

--- ruby/practice.rb
+++ ruby/practice.rb
@@ -1,10 +1,8 @@
-a = 3.14 * 3.14
-b = 4.55 * 4.55
-c = 234.565433695849 * 234.565433695849
+puts "How old are you?"
 
+user_age = gets.chomp.to_i
 
-
-
-puts a
-puts b
-puts c
+puts "In 10 years you will be #{user_age + 10}"
+puts "In 20 years you will be #{user_age + 20}"
+puts "In 30 years you will be #{user_age + 30}"
+puts "In 40 years you will be #{user_age + 40}"
